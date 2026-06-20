42c210f894f8351e99e123e3ed3beb7ac4b441ef
Bart de Lange <bart.de.lange32@gmail.com>
2026-06-20 20:14:13 +0200

    chore: initialize project :tada:

---
+++ frontend/src/app/routes/AppRouter.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AppShell } from "../layout/AppShell";
+import { LibraryPage } from "../../pages/library/LibraryPage";
+
+export function AppRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<AppShell />}>
+          <Route index element={<LibraryPage />} />
+          <Route path="/objects" element={<LibraryPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
---
+++ frontend/src/pages/library/LibraryPage.tsx
@@ -0,0 +1,8 @@
+export function LibraryPage() {
+  return (
+    <div className="p-4">
+      <h1 className="text-2xl font-bold mb-4">Library Page</h1>
+      <p>Welcome to the Library Page!</p>
+    </div>
+  );
+}
